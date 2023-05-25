e28b370eaf122cccd595dbe288768ac4c90f1b8d
Peter Illes <peter.illes@gofurther.digital>
2023-05-25 11:20:19 +0200

    Updated CTooltipIcon icon to a more cleaner look.

--- src/ts/components/CTooltipIcon.vue
+++ src/ts/components/CTooltipIcon.vue
@@ -1,12 +1,14 @@
 <template>
-    <a class="c-tooltip-icon after:absolute after:content-['i'] after:font-bold after:font-mono after:leading-none after:left-1/2 after:text-xs after:text-white after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2 bg-highlight cursor-pointer flex h-5 mx-2 relative rounded-full shrink-0 w-5" v-on:mouseout="close" v-on:mouseover="open">
+    <span class="c-tooltip-icon cursor-pointer flex h-5 mx-2 relative rounded-full shrink-0 w-5" v-on:mouseout="close" v-on:mouseover="open">
+        <c-icon class="h-5 mr-2 stroke-highlight stroke-2 w-5" icon="info" />
         <c-tooltip v-bind:active="active as boolean" v-bind:position="position">
             <slot></slot>
         </c-tooltip>
-    </a>
+    </span>
 </template>
 
 <script setup lang="ts">
+    import CIcon from './CIcon.vue';
     import CTooltip from './CTooltip.vue';
     import { PropType, ref } from 'vue';
     import { PositionEnum } from '../enums/PositionEnum';
